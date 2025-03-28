a13c338b918e0a12d35c71aac68de9aef6cf4816
Zoarial94 <HWB94@pm.me>
2025-03-28 16:38:28 -0400

    Refactored to produce smoother frames.
    Note that there's currently minimal/no memory limiting, so it will attempt to load a large amount of data at once

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,29 @@
-use magick_rust::magick_wand_genesis;
-use std::fs::{DirBuilder, DirEntry};
+use magick_rust::{magick_wand_genesis};
 use std::path::Path;
 use std::process::Command;
 use std::sync::{LazyLock, Once};
 use std::env;
-use std::fmt::format;
-use magick_rust::CompressionType::Group4;
+use circular_buffer::CircularBuffer;
 use tokio::task;
 
 static START: Once = Once::new();
-static GROUP_SIZE: u8 = 2;
+static GROUP_SIZE: u8 = 3;
 static COMBINED_DIR: LazyLock<String> = LazyLock::new(|| {
     format!("combined-{:01}", GROUP_SIZE)
 });
 
 struct ImageMerge {
-    img_vec: Vec<DirEntry>,
+    img_vec: Vec<String>,
 }
 
 impl ImageMerge {
-    fn add_image(&mut self, dir_entry: DirEntry) -> Result<(), &str> {
-        if !self.is_full() {
-            self.img_vec.push(dir_entry);
-            return Ok(())
-        }
-        Err("Vec full")
-    }
-
-    fn is_full(&self) -> bool {
-        self.img_vec.len() >= GROUP_SIZE as usize
-    }
-
-    fn is_empty(&self) -> bool {
-        self.img_vec.is_empty()
-    }
-
     fn process(&mut self, counter: u32) {
         let mut command = Command::new("magick");
         let ret = self.img_vec.iter().fold(String::with_capacity((GROUP_SIZE * 10) as usize), |acc_str, img| {
-            let file_name = img.file_name().to_str().expect("Error").to_owned();
+            let file_name = img;
             //self.wand.read_image(file_name.as_str()).expect("TODO: panic message");
             command.arg(file_name.to_owned());
-            acc_str + file_name.as_str() + " "
+            acc_str + file_name + " "
         });
         //fs::write("0.jpg", self.wand.evaluate_image(Mean, 0.0).unwrap()).expect("TODO: panic message");
 
@@ -51,11 +33,6 @@ impl ImageMerge {
         command.output().expect("TODO: panic message");
 
     }
-
-    fn reset(&mut self) {
-        self.img_vec = vec![];
-    }
-
 }
 
 #[tokio::main]
@@ -67,6 +44,7 @@ async fn main() {
 
     let mut threads: Vec<_> = vec![];
     let mut counter = 0;
+    let mut img_buffer : CircularBuffer<{ GROUP_SIZE as usize }, String> = CircularBuffer::new();
 
     let path = env::args().skip(1).next().unwrap();
     let img_folder = Path::new(&path);
@@ -74,47 +52,36 @@ async fn main() {
 
     let iter = img_folder.read_dir().expect("read dir error").peekable();
 
-    let mut image_merge = ImageMerge{
-        img_vec: vec![],
-    };
-    
+
     if !std::fs::exists(COMBINED_DIR.as_str()).expect("TODO: panic message") {
         std::fs::create_dir(COMBINED_DIR.as_str()).expect("TODO: panic message");
     }
 
     for img in iter {
-        if !image_merge.is_full() {
-            image_merge.add_image(img.unwrap()).expect("Err");
-        }
-
-        if image_merge.is_full() {
-            println!("Queuing job {:04}", counter);
-            let handle = task::spawn(async move {
-                image_merge.process(counter);
-            });
-            image_merge = ImageMerge{
-                img_vec: vec![],
-            };
-            threads.push(handle);
-            counter = counter + 1;
-        }
         
-        if counter >= 300 {
-            break
-        }
+        let file_name = img.unwrap().file_name().into_string().unwrap();
+        img_buffer.push_back(file_name);
         
-    }
-
-    if !image_merge.is_empty() {
+        let mut image_merge = ImageMerge {
+            img_vec: img_buffer.iter().cloned().collect(), 
+        };
+        
+        println!("Queuing job {:04}", counter);
         let handle = task::spawn(async move {
             image_merge.process(counter);
         });
         threads.push(handle);
+        counter = counter + 1;
+        
+        // if counter >= 300 {
+        //     break
+        // }
+        
     }
 
-    for handle in threads {
-        handle.await.unwrap();
-    }
+    // for handle in threads {
+    //     handle.await.unwrap();
+    // }
     
 }
 
